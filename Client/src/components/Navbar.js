795365e294c11e856b81dad331c6920408da1689
Daniel Milan <danielmilanvera@gmail.com>
2022-03-09 18:40:45 +0100

    NavBar and Signup isHost

--- Client/src/components/Navbar.js
+++ Client/src/components/Navbar.js
@@ -2,38 +2,76 @@ import HomePage from "../pages/HomePage";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "./../context/auth.context";
+import * as Bootstrap from "react-bootstrap";
 
 function Navbar() {
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
 
   return (
-    <nav className={"navBar"}>
-      <Link to={`/`}>
-        <h3>Go Home</h3>
-      </Link>
+    <Bootstrap.Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Bootstrap.Container>
+        <Link to={"/"}>
+          {" "}
+          <Bootstrap.Navbar.Brand>Camelcasers</Bootstrap.Navbar.Brand>
+        </Link>
+        <Bootstrap.Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Bootstrap.Navbar.Collapse id="responsive-navbar-nav">
+          <Bootstrap.Nav className="me-auto">
+            {isLoggedIn && (
+              <>
+                <Link to="/projects">
+                {" "}
+                  <Bootstrap.Nav.Link href=".">MyEvents</Bootstrap.Nav.Link>
+                </Link>
+                <Link to="/projects">
+                {" "}
+                  <Bootstrap.Nav.Link href=".">Messages</Bootstrap.Nav.Link>
+                </Link>
+                <Bootstrap.Nav.Link onClick={logOutUser}>
+                  Logout
+                </Bootstrap.Nav.Link>
+              </>
+            )}
 
-      {isLoggedIn && (
-        <>
-          <Link to="/projects">
-            <button>Projects</button>
-          </Link>
-          <button onClick={logOutUser}>Logout</button>
-        </>
-      )}
+            <Bootstrap.NavDropdown
+              title="Dropdown"
+              id="collasible-nav-dropdown"
+            >
+              <Bootstrap.NavDropdown.Item href="#action/3.1">
+                Action
+              </Bootstrap.NavDropdown.Item>
+              <Bootstrap.NavDropdown.Item href="#action/3.2">
+                Another action
+              </Bootstrap.NavDropdown.Item>
+              <Bootstrap.NavDropdown.Item href="#action/3.3">
+                Something
+              </Bootstrap.NavDropdown.Item>
+              <Bootstrap.NavDropdown.Divider />
+              <Bootstrap.NavDropdown.Item href="#action/3.4">
+                Separated link
+              </Bootstrap.NavDropdown.Item>
+            </Bootstrap.NavDropdown>
+          </Bootstrap.Nav>
 
-      {!isLoggedIn && (
-        <>
-          <Link to="/signup">
-            {" "}
-            <button>Sign Up</button>{" "}
-          </Link>
-          <Link to="/login">
-            {" "}
-            <button>Login</button>{" "}
-          </Link>
-        </>
-      )}
-    </nav>
+          <Bootstrap.Nav>
+            {!isLoggedIn && (
+              <>
+                <Link to="/signup">
+                  {" "}
+                  <Bootstrap.Nav.Link eventKey={2} href=".">
+                    Sing Up
+                  </Bootstrap.Nav.Link>
+                </Link>
+
+                <Link to="/login">
+                  <Bootstrap.Nav.Link href=".">Log In</Bootstrap.Nav.Link>{" "}
+                </Link>
+              </>
+            )}
+          </Bootstrap.Nav>
+        </Bootstrap.Navbar.Collapse>
+      </Bootstrap.Container>
+    </Bootstrap.Navbar>
   );
 }
 
