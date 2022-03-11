c5e9c359b8a8aff4b630a59014834a08ccf3b71f
Daniel Milan <danielmilanvera@gmail.com>
2022-03-11 11:14:49 +0100

    Cosas que miki borro

--- Client/src/App.js
+++ Client/src/App.js
@@ -8,56 +8,77 @@ import EventListPage from "./pages/EventListPage";
 import EditEventPage from "./pages/EditEventPage";
 import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
-import IsPrivate from "./components/IsPrivate";  
-import IsAnon from "./components/IsAnon";  
+import IsPrivate from "./components/IsPrivate";
+import IsAnon from "./components/IsAnon";
 import { AuthContext } from "./context/auth.context";
-import ArtistProfilePage from "./pages/ArtistProfilePage"
-import HostProfilePage from "./pages/HostProfilePage"
+import ArtistProfilePage from "./pages/ArtistProfilePage";
+import HostProfilePage from "./pages/HostProfilePage";
 import CreateEventPage from "./pages/CreateEventPage";
-import HostFormPage from "./pages/HostFormPage"
-import ArtistFormPage from "./pages/ArtistFormPage"
+import HostFormPage from "./pages/HostFormPage";
+import ArtistFormPage from "./pages/ArtistFormPage";
 import EventDetailsPage from "./pages/EventDetailsPage";
-
-
-
+import HostListPage from "./pages/HostFormPage"
+import ArtistListPage from "./pages/ArtistListPage"
 
 function App() {
-  const {loggedHost, loggedArtist} = useContext(AuthContext)
+  const { loggedHost, loggedArtist } = useContext(AuthContext);
   return (
     <div>
       {/*  ADD <Navbar>, <Routes> & <Route>  */}
       <Navbar />
 
       <Routes>
-        <Route path="/" element={ <HomePage /> } />
-        <Route path="/events" element={<EventListPage />}/>
-        <Route path="/events/create" element={ <CreateEventPage /> } />
-        <Route path="/events/:eventId" element={ <EventDetailsPage /> } />
-        <Route path="/events/edit/:eventId" element={ <EditEventPage /> } />
-       
+        <Route path="/" element={<HomePage />} />
+        <Route path="/events" element={<EventListPage />} />
+        <Route path="/events/create" element={<CreateEventPage />} />
+        <Route path="/events/:eventId" element={<EventDetailsPage />} />
+        <Route path="/events/edit/:eventId" element={<EditEventPage />} />
+
+        <Route path="/hostList" element={<HostListPage />} />
+        <Route path="/artistList" element={<ArtistListPage />} />
         
+        {loggedHost && (
+          <Route path="/profile/:profileId" element={<HostProfilePage />} />
+        )}
 
-      {loggedHost && (
-        <Route path="/profile/:profileId" element={ <HostProfilePage /> }/>
-      )}
+        {loggedHost && (
+          <Route path="/profile/:profileId/edit" element={<HostFormPage />} />
+        )}
 
-      {loggedHost &&
-        <Route path="/profile/:profileId/edit" element={ <HostFormPage /> }/>
-      }
+        {loggedArtist && (
+          <Route path="/profile/:profileId/edit" element={<ArtistFormPage />} />
+        )}
 
+        {loggedArtist && (
+          <Route
+            path="/profile/:profileId"
+            element={
+              <IsPrivate>
+                {" "}
+                <ArtistProfilePage />{" "}
+              </IsPrivate>
+            }
+          />
+        )}
 
-
-      {loggedArtist &&
-        <Route path="/profile/:profileId/edit" element={<ArtistFormPage/> }/>
-      }
-      
-
-      {loggedArtist && 
-        <Route path="/profile/:profileId" element={<IsPrivate> <ArtistProfilePage/> </IsPrivate>}/>
-      }
-
-        <Route path="/signup" element={ <IsAnon> <SignupPage /> </IsAnon> } />
-        <Route path="/login" element={ <IsAnon> <LoginPage /> </IsAnon> } />
+        <Route
+          path="/signup"
+          element={
+            <IsAnon>
+              {" "}
+              <SignupPage />{" "}
+            </IsAnon>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <IsAnon>
+              {" "}
+              <LoginPage />{" "}
+            </IsAnon>
+          }
+        />
       </Routes>
     </div>
   );
