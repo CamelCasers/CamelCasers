ceec89058a789d5fa13bca0d47430efb4e495c38
Miki Blasco <blascom@7@gmail.com>
2022-03-13 13:40:58 +0100

    Formularios Bootsrap

--- Client/src/pages/CreateEventPage.js
+++ Client/src/pages/CreateEventPage.js
@@ -8,7 +8,7 @@ const API_URL = "http://localhost:5005";
 
 function CreateEventPage(props) {
   const { user } = useContext(AuthContext);
-  console.log("user 0=>", user)
+  console.log("user 0=>", user);
 
   const [title, setTitle] = useState("");
   const [date, setDate] = useState("");
@@ -20,85 +20,183 @@ function CreateEventPage(props) {
   const [timeRange, setTimeRange] = useState("");
   const [equipment, setEquipment] = useState("");
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const requestBody = { title, date, location, images, videos, musicStyle, description, timeRange, equipment, user };
-   
+    const requestBody = {
+      title,
+      date,
+      location,
+      images,
+      videos,
+      musicStyle,
+      description,
+      timeRange,
+      equipment,
+      user,
+    };
+
     // Get the token from the localStorage
-    const storedToken = localStorage.getItem('authToken');
-   
+    const storedToken = localStorage.getItem("authToken");
+
     // Send the token through the request "Authorization" Headers
     axios
-      .post(
-      `${API_URL}/api/events`,
-      requestBody,
-      { headers: { Authorization: `Bearer ${storedToken}` } }
-    )
+      .post(`${API_URL}/api/events`, requestBody, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
       .then((response) => {
-      // Reset the state
-      setTitle("");
-      setDate("");
-      setLocation("");
-      setImages("");
-      setVideos("");
-      setMusicStyle("");
-      setDescription("");
-      setTimeRange("");
-      setEquipment("");
-      //props.refreshEvents();
+        // Reset the state
+        setTitle("");
+        setDate("");
+        setLocation("");
+        setImages("");
+        setVideos("");
+        setMusicStyle("");
+        setDescription("");
+        setTimeRange("");
+        setEquipment("");
+        //props.refreshEvents();
 
-      navigate(`/events/`)
-    })
+        navigate(`/events/`);
+      })
       .catch((error) => console.log(error));
   };
 
   return (
-    <div className="CreateEvent">
-
+    <div>
       <form onSubmit={handleSubmit}>
-        <label>Title:</label>
-        <input type="text" name="title" value={title} onChange={(e) => setTitle(e.target.value)}/>
-        <br/>
-        <label>Date:</label>
-        <input type="date" name="date" value={date} onChange={(e) => setDate(e.target.value)}/>
-        <br/>
-        <label>Location:</label>
-        <input type="text" name="location" value={location} onChange={(e) => setLocation(e.target.value)}/>
-        <br/>
-        <label>Images:</label>
-        <input type="file" name="images" value={images} onChange={(e) => setImages(e.target.value)}/>
-        <br/>
-        <label>Videos:</label>
-        <input type="file" name="videos" value={videos} onChange={(e) => setVideos(e.target.value)}/>
-        <br/>
-        <label>Music Style:</label>
-        <select type="text" name="musicStyle" value={musicStyle} onChange={(e) => setMusicStyle(e.target.value)}>
-                  <option value="rock">Rock</option>
-                  <option value="reggae">Reggae</option>
-                  <option value="Pop">Pop</option>
-                  <option value="romantic">Romantic</option>
-                  <option value="party">Party</option>
-                  <option value="swing">Swing</option>
-                  <option value="heavy">Heavy</option>
-                  <option value="others">Others</option>
-        </select>
-        <br/>
-        <label>Description:</label>
-        <input type="text" name="description" value={description} onChange={(e) => setDescription(e.target.value)}/>
-        <br/>
-        <label>Time Range:</label>
-        <input type="time" min="" max name="timeRange" value={timeRange} onChange={(e) => setTimeRange(e.target.value)}/>
-        <br/>
-        <label>Equipment:</label>
-        <input type="text" name="equipment" value={equipment} onChange={(e) => setEquipment(e.target.value)}/>
-        <br/>
+        <div className="row mb-3">
+          <label>Event title: </label>
+          <div className="col-sm-10">
+          <input
+            className="form-control"
+            type="text"
+            name="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          </div>
+        </div>
+        <div className="row mb-3">
+          <label>Date: </label>
+          <div className="col-sm-10">
+          <input
+            className="form-control"
+            type="date"
+            name="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+          />
+          </div>
+        </div>
+        <div className="row mb-3">
+          <label>Time:</label>
+          <div className="col-sm-10">
+          <input
+            type="time"
+            min=""
+            max
+            name="timeRange"
+            value={timeRange}
+            onChange={(e) => setTimeRange(e.target.value)}
+          />
+          </div>
+        </div>
+        <div className="row mb-3">
+          <label>Location: </label>
+          <div className="col-sm-10">
+          <input
+            className="form-control"
+            type="text"
+            name="location"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+          />
+          </div>
+        </div>
+        <div className="row mb-3">
+          <label>Description:</label>
+          <div className="col-sm-10">
+          <textarea
+            className="form-control"
+            type="text"
+            name="description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+          </div>
+        </div>
+                <div className="row mb-3">
+          <label>Music Style: </label>
+          <div className="col-sm-10">
+          <select
+            type="text"
+            name="musicStyle"
+            value={musicStyle}
+            onChange={(e) => setMusicStyle(e.target.value)}
+          >
+            <option value="rock">Rock</option>
+            <option value="reggae">Reggae</option>
+            <option value="Pop">Pop</option>
+            <option value="romantic">Romantic</option>
+            <option value="party">Party</option>
+            <option value="swing">Swing</option>
+            <option value="heavy">Heavy</option>
+            <option value="others">Others</option>
+          </select>
+          </div>
+        </div>
 
-        <button type="submit"> Create Event</button>
+        <div className="row mb-3">
+          <label>Equipment Provided:</label>
+          <div className="col-sm-10">
+          <input
+            className="form-control"
+            type="text"
+            name="equipment"
+            value={equipment}
+            onChange={(e) => setEquipment(e.target.value)}
+          />
+          </div>
+        </div>
+
+
+        <div className="row mb-3">
+          <label>Event Images: </label>
+          <div className="col-sm-10">
+          <input
+            type="file"
+            name="images"
+            value={images}
+            onChange={(e) => setImages(e.target.value)}
+          />
+          </div>
+        </div>
+
+        <div className="row mb-3">
+          <label>Upload Event Videos: </label>
+          <div className="col-sm-10">
+          <input
+            className="form-control"
+            type="file"
+            name="videos"
+            value={videos}
+            onChange={(e) => setVideos(e.target.value)}
+          />
+          </div>
+        </div>
+
+
+
+
+        <button className="btn btn-secondary" type="submit">
+          {" "}
+          Create Event
+        </button>
       </form>
     </div>
   );
 }
 
-export default CreateEventPage
+export default CreateEventPage;
--- Client/src/pages/EditEventPage.js
+++ Client/src/pages/EditEventPage.js
@@ -18,21 +18,20 @@ function EditEventPage(props) {
   const [timeRange, setTimeRange] = useState("");
   const [equipment, setEquipment] = useState("");
 
-  // Get the URL parameter `:eventId` 
-  const { eventId } = useParams();      
-  const navigate = useNavigate();  
-  
-  const storedToken = localStorage.getItem("authToken");
-    
+  // Get the URL parameter `:eventId`
+  const { eventId } = useParams();
+  const navigate = useNavigate();
 
- // This effect will run after the initial render and each time
- // the event id coming from URL parameter `eventtId` changes
-  
-  useEffect(() => {                                
+  const storedToken = localStorage.getItem("authToken");
+
+  // This effect will run after the initial render and each time
+  // the event id coming from URL parameter `eventtId` changes
+
+  useEffect(() => {
     axios
-      .get(`${API_URL}/api/event/${eventId}`,  
-      { headers: { Authorization: `Bearer ${storedToken}` } }
-      )
+      .get(`${API_URL}/api/event/${eventId}`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
       .then((response) => {
         /* 
           We update the state with the project data coming from the response.
@@ -43,97 +42,188 @@ function EditEventPage(props) {
         setDescription(oneEvent.description);
       })
       .catch((error) => console.log(error));
-    
   }, [eventId, storedToken]);
 
-  const handleFormSubmit = (e) => {                    
+  const handleFormSubmit = (e) => {
     e.preventDefault();
     // Create an object representing the body of the PUT request
-    const requestBody = { title, date, location, images, videos, musicStyle, description, timeRange, equipment };
+    const requestBody = {
+      title,
+      date,
+      location,
+      images,
+      videos,
+      musicStyle,
+      description,
+      timeRange,
+      equipment,
+    };
     console.log("requestBody =>", requestBody);
- 
+
     // Make a PUT request to update the project
     axios
-      .put(`${API_URL}/api/events/${eventId}`, requestBody, { headers: { Authorization: `Bearer ${storedToken}` } })
+      .put(`${API_URL}/api/events/${eventId}`, requestBody, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
       .then((response) => {
         console.log("2=>", requestBody);
         // Once the request is resolved successfully and the project
         // is updated we navigate back to the details page
-        navigate(`/events/${eventId}`)
+        navigate(`/events/${eventId}`);
       });
   };
 
   const deleteEvent = () => {
     axios
-    .delete(`${API_URL}/api/events/${eventId}`, 
-    { headers: { Authorization: `Bearer ${storedToken}` }}
-    )
-    .then(()=>{
-      //Once the delete request is resvolved succesfully 
-      // navigate back to the list of projects
-      navigate("/events");
-    })
-    .catch((err)=> console.log(err));
+      .delete(`${API_URL}/api/events/${eventId}`, {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      .then(() => {
+        //Once the delete request is resvolved succesfully
+        // navigate back to the list of projects
+        navigate("/events");
+      })
+      .catch((err) => console.log(err));
   };
 
-  function imgUpload(url){
-    if(url){
-      setImages(images.concat(url))
+  function imgUpload(url) {
+    if (url) {
+      setImages(images.concat(url));
     }
-
   }
 
-  
   return (
-    <div className="EditEventPage">
-      <h3>Edit the Event</h3>
-
+    <div>
       <form onSubmit={handleFormSubmit}>
-      <label>Title:</label>
-        <input type="text" name="title" value={title} onChange={(e) => setTitle(e.target.value)}/>
-        <br/>
-        <label>Date:</label>
-        <input type="date" name="date" value={date} onChange={(e) => setDate(e.target.value)}/>
-        <br/>
-        <label>Location:</label>
-        <input type="text" name="location" value={location} onChange={(e) => setLocation(e.target.value)}/>
-        <br/>
-    
-    <CloudinaryUpload imgUpload={imgUpload} images={images}/>
+        <div className="row mb-3">
+          <label>Event title: </label>
+          <div className="col-sm-10">
+            <input
+              className="form-control"
+              type="text"
+              name="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div className="row mb-3">
+          <label>Date: </label>
+          <div className="col-sm-10">
+            <input
+              className="form-control"
+              type="date"
+              name="date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div className="row mb-3">
+          <label>Time:</label>
+          <div className="col-sm-10">
+            <input
+              type="time"
+              name="timeRange"
+              value={timeRange}
+              onChange={(e) => setTimeRange(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div className="row mb-3">
+          <label>Address: </label>
+          <div className="col-sm-10">
+            <input
+              className="form-control"
+              type="text"
+              name="location"
+              value={location}
+              onChange={(e) => setLocation(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div className="row mb-3">
+          <label>Description:</label>
+          <div className="col-sm-10">
+            <textarea
+              className="form-control"
+              type="text"
+              name="description"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div className="row mb-3">
+          <label>Music Style: </label>
+          <div className="col-sm-10">
+            <select
+              type="text"
+              name="musicStyle"
+              value={musicStyle}
+              onChange={(e) => setMusicStyle(e.target.value)}
+            >
+              <option value="rock">Rock</option>
+              <option value="reggae">Reggae</option>
+              <option value="Pop">Pop</option>
+              <option value="romantic">Romantic</option>
+              <option value="party">Party</option>
+              <option value="swing">Swing</option>
+              <option value="heavy">Heavy</option>
+              <option value="chill">Chill</option>
+              <option value="chill">Classic</option>
+              <option value="chill">Jazz</option>
+              <option value="others">Others</option>
+            </select>
+          </div>
+        </div>
+
+        <div className="row mb-3">
+          <label>Equipment Provided:</label>
+          <div className="col-sm-10">
+            <input
+              className="form-control"
+              type="text"
+              name="equipment"
+              value={equipment}
+              onChange={(e) => setEquipment(e.target.value)}
+            />
+          </div>
+        </div>
+
+        <div className="mb-3">
+          <label for="formFile" className="form-label">
+            {" "}
+            Event Images:{" "}
+          </label>
+          <CloudinaryUpload
+            className="form-control form-control-sm"
+            type="file"
+            id="formFile"
+            imgUpload={imgUpload}
+            images={images}
+          />
+        </div>
 
         <label>Videos:</label>
-        <input type="file" name="videos" value={videos} onChange={(e) => setVideos(e.target.value)}/>
-        <br/>
-        <label>Music Style:</label>
-        <select type="text" name="musicStyle" value={musicStyle} onChange={(e) => setMusicStyle(e.target.value)}>
-                  <option value="rock">Rock</option>
-                  <option value="reggae">Reggae</option>
-                  <option value="Pop">Pop</option>
-                  <option value="romantic">Romantic</option>
-                  <option value="party">Party</option>
-                  <option value="swing">Swing</option>
-                  <option value="heavy">Heavy</option>
-                  <option value="others">Others</option>
-        </select>
-        <br/>
-        <label>Description:</label>
-        <input type="text" name="description" value={description} onChange={(e) => setDescription(e.target.value)}/>
-        <br/>
-        <label>Time Range:</label>
-        <input type="time" min="" max name="timeRange" value={timeRange} onChange={(e) => setTimeRange(e.target.value)}/>
-        <br/>
-        <label>Equipment:</label>
-        <input type="text" name="equipment" value={equipment} onChange={(e) => setEquipment(e.target.value)}/>
-        <br/>
-
-        <button type="submit">Update Event</button>
-      </form>
-         
-         <button onClick={deleteEvent}>Delete Event</button>
-       
-    </div>
-
+        <input
+          type="file"
+          name="videos"
+          value={videos}
+          onChange={(e) => setVideos(e.target.value)}
+        />
+       <hr/>
     
+        <button  className="btn btn-secondary" type="submit">Update Event</button>
+      </form>
+       <br/>
+      <button className="btn btn-danger" onClick={deleteEvent}>Delete Event</button>
+    </div>
   );
 }
 
