4bb0d3096247076dce1dd8e0381d5b0272c82a77
Miki Blasco <blascom@7@gmail.com>
2022-03-14 12:38:44 +0100

    event list

--- Client/src/components/EventCard.js
+++ Client/src/components/EventCard.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { Card, Button, Container } from "react-bootstrap";
 
 
 function EventCard(props) {
@@ -6,24 +7,25 @@ function EventCard(props) {
   const {_id, title, date, location, images, videos, musicStyle, description, timeRange, equipment, host } = props
 //console.log(host)
     return (
-        <div className={"EventCard card"}>      
-
-          <Link to={`/events/${_id}`} eventDetails = {title}>
-            <h3>Event Name: {title}</h3>
-          </Link>
-          {host && (
-          <p style={{ maxWidth: "400px" }}>Event host: {host.name} </p>
-
-          )}
-          <p style={{ maxWidth: "400px" }}>Date: {date} </p>
-          <p style={{ maxWidth: "400px" }}>Location: {location} </p>
-
-          {images.map((img)=>(
-            <img src={img} alt="Pic" width={300} />))}
-
-     
+      <Container>
+      <div className="centerItemsContainer">
+        <Card style={{ width: "20rem" }}  className="backgroundMiki text-white centerItemsContainer">
+          <Card.Img variant="top" src={images[0]} className="rounded mx-auto d-block"/>
+          <Card.Body>
+            <Card.Title>{title}</Card.Title>
+            <Card.Text>{description}</Card.Text>
+            <Card.Text>Address: {location}</Card.Text>
+            <Card.Text>Music Style: {musicStyle}</Card.Text>
+            <Button variant="primary" as={Link} to={`/profileHost/${_id}`}>
+              Details
+            </Button>
+          </Card.Body>
           
-        </div>
+        </Card>
+        <br/>
+        
+      </div>
+    </Container>
       );
     }
 
