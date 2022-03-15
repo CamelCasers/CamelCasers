17f843f4e350d975a031a533daa9cdf58b7fe859
Daniel Milan <danielmilanvera@gmail.com>
2022-03-15 14:17:35 +0100

    Artist messages

--- Client/src/pages/ArtistMessagesPage.js
+++ Client/src/pages/ArtistMessagesPage.js
@@ -3,10 +3,8 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
-import {Card, Container} from "react-bootstrap"
+import { Card, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
-
-
 
 const API_URL = "http://localhost:5005";
 export default function ArtistMessagesPage() {
@@ -20,10 +18,8 @@ export default function ArtistMessagesPage() {
     images: [],
     videos: [],
     events: [],
-    pendingEvents: []
+    pendingEvents: [],
   });
-
-  
 
   const storedToken = localStorage.getItem("authToken");
   function getArtist() {
@@ -38,47 +34,76 @@ export default function ArtistMessagesPage() {
       .catch((error) => console.log(error));
   }
 
-
   useEffect(() => {
     getArtist();
   }, []);
+
   return (
     <div>
-      
-   
-     {artist.pendingEvents && (<h1>Pending Events</h1>)}  
-        
-      {artist.pendingEvents?.map((event)=>(
-        
-        <div>
-  <Container>
-      <div className="centerItemsContainer">
-        <div className="centerItemsContainer backgroundEventCard text-white" style={{ width: "20rem" }}  >
-          <Card.Title>{event.date}</Card.Title>
-          <Card.Img variant="top" src={event.images[0]} style={{}}/>
-          <Card.Body>         
-            <Card.Title>{event.title}</Card.Title>
-            <Card.Text>Music Style: {event.musicStyle}</Card.Text>
-            <Card.Text>Address: {event.location}</Card.Text>
-              
-            
-            <Link to={`/events/${event._id}`}>
-            <button className="btn btn-outline-warning ">
-              See Details
-            </button>
-            </Link>
-            
-          </Card.Body>
-          
-        </div>       
-      </div>
-    </Container>
+      <div>
+        {artist.pendingEvents && <h1>Pending Events</h1>}
+        {artist.pendingEvents.length == 0 && <p>Not pending events to confirm</p>}
 
-          
-        </div>
 
-      ))}
+        {artist.pendingEvents?.map((event) => (
+          <div>
+            <Container>
+              <div className="centerItemsContainer">
+                <div
+                  className="centerItemsContainer backgroundEventCard text-white"
+                  style={{ width: "20rem" }}
+                >
+                  <Card.Title>{event.date}</Card.Title>
+                  {console.log("Pending events", event)}
+                  <Card.Img variant="top" src={event.images[0]} style={{}} />
+                  <Card.Body>
+                    <Card.Title>{event.title}</Card.Title>
+                    <Card.Text>Music Style: {event.musicStyle}</Card.Text>
+                    <Card.Text>Address: {event.location}</Card.Text>
+
+                    <Link to={`/events/${event._id}`}>
+                      <button className="btn btn-outline-warning ">
+                        See Details
+                      </button>
+                    </Link>
+                  </Card.Body>
+                </div>
+              </div>
+            </Container>
+          </div>
+        ))}
       </div>
-  )
+      <div>
+        {artist.events && <h1>Confirmed Events</h1>}
+
+        {artist.events?.map((event) => (
+          <div>
+            <Container>
+              <div className="centerItemsContainer">
+                <div
+                  className="centerItemsContainer backgroundEventCard text-white"
+                  style={{ width: "20rem" }}
+                >
+                  <Card.Title>{event.date}</Card.Title>
+                  {console.log("confirmed Eents", artist.events)}
+                  <Card.Img variant="top" src={event.images[0]} style={{}} />
+                  <Card.Body>
+                    <Card.Title>{event.title}</Card.Title>
+                    <Card.Text>Music Style: {event.musicStyle}</Card.Text>
+                    <Card.Text>Address: {event.location}</Card.Text>
+
+                    <Link to={`/events/${event._id}`}>
+                      <button className="btn btn-outline-warning ">
+                        See Details
+                      </button>
+                    </Link>
+                  </Card.Body>
+                </div>
+              </div>
+            </Container>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
-
